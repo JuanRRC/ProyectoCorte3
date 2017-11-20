875263e23558e32ff7efec54306fc20be154c569
juan <delben555@gmail.com>
2017-11-19 23:46:40 -0500

    metodos clase coordenada
    se añaden tres metodos para tres tipos posibles de coordenadas y sus respectivos get y set

--- src/Logica2/Coordenada.java
+++ src/Logica2/Coordenada.java
@@ -10,5 +10,52 @@ package Logica2;
  * @author JUAN DAVID
  */
 public class Coordenada {
-    
+    private float x;
+    private float y;
+    /**
+     * metodo para coordenada sin atrivutos
+     */
+    public Coordenada(){
+        this.x =0;
+        this.y =0;
+        
+    } 
+     /**
+     * controla las coordenas "movimientos" que el usuario dessee
+     * @param x
+     * @param y 
+     */
+    public Coordenada(float x, float y ){
+        this.x =x;
+        this.y =y;
+        
+    }
+    /**
+     * manejara coordenas nuevas 
+     * @param nueva 
+     */
+       public Coordenada(Coordenada nueva){
+        this.x =nueva.x;
+        this.y =nueva.y;
+        
+    }
+       /**
+        * 
+        * metodos que daran y retornaran las coordenanas
+        */
+       public float getX() {
+        return this.x;
+    }
+
+    public void setX(float x) {
+        this.x = x;
+    }
+
+    public float getY() {
+        return this.y;
+    }
+
+    public void setY(float y) {
+        this.y = y;
+    }
 }
