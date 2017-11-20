3df60ef8944a7bd254568b2d6b060eb50df059bf
juan <delben555@gmail.com>
2017-11-20 14:28:36 -0500

    Constructores de circulo
    envian y reciben parametros que se usanan el clase
     circulo grafico para pintar las las balas

--- src/Logica2/Circulo.java
+++ src/Logica2/Circulo.java
@@ -9,6 +9,50 @@ package Logica2;
  *
  * @author JUAN DAVID
  */
-public class Circulo {
-    
+public class Circulo extends Coordenada{
+    //radio del circulo
+     private float radio;
+     /**
+      * constructor
+      */
+    public Circulo(){
+        super();
+        this.radio  = 0;
+        
+    }
+    /**
+     * recibe radio y coordenada
+     * @param nueva
+     * @param r 
+     */
+       public Circulo(Coordenada nueva, float r){
+        super(nueva);
+        this.radio = r;
+    }
+       /**
+        * constructor con circulo 
+        * @param cir 
+        */
+        public Circulo(Circulo cir){
+        super(cir);
+        this.radio = cir.radio;
+    }
+        /**
+         * se usara por si se llega a necesesitar en otra parte el 
+         * radio de el circulo
+         */
+    public float getRadio() {
+        return radio;
+    }
+
+    public void setRadio(float r) {
+        this.radio = r;
+    }
+    /**
+     * retornara la coordenada con el centro de el circulo
+     */
+     public Coordenada getCentro(){
+        Coordenada nueva = new Coordenada (this.getX(),this.getY());
+        return nueva;
+    }
 }
