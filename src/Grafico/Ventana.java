0fe13f94406bfec4c024c83323d870c3f89a42ad
juan <delben555@gmail.com>
2017-11-19 23:34:30 -0500

    constrctor de clase ventana
    se crea constructor y se llama a la clase  main aun no hay nada fucional no se ha añadido al  main original

--- src/Grafico/Ventana.java
+++ src/Grafico/Ventana.java
@@ -5,10 +5,55 @@
  */
 package Grafico;
 
+import java.awt.event.WindowEvent;
+import java.awt.event.WindowListener;
+import javax.swing.JFrame;
+
 /**
  *
  * @author JUAN DAVID
  */
-public class Ventana {
+/**
+ * 
+ * se implementa el WindowListener para poder cerrar la ventana
+ */
+public class Ventana extends JFrame implements WindowListener{
+    /**
+     * llama al constructor padre y se fijahasta recibir un string
+     * @param title 
+     */
+      public Ventana (String title){
+        super(title);
+        setSize(800,600);
+        addWindowListener(this);
+    }
+
+     @Override
+    public void windowOpened(WindowEvent we) {
+    }
+
+    @Override
+    public void windowClosing(WindowEvent we)  { System.exit(0);
+    }
+
+    @Override
+    public void windowClosed(WindowEvent we) {
+    }
+
+    @Override
+    public void windowIconified(WindowEvent we) {
+    }
+
+    @Override
+    public void windowDeiconified(WindowEvent we) {
+    }
+
+    @Override
+    public void windowActivated(WindowEvent we) {
+    }
+
+    @Override
+    public void windowDeactivated(WindowEvent we) {
+    }
     
 }
--- src/Principal/main.java
+++ src/Principal/main.java
@@ -5,12 +5,28 @@
  */
 package Principal;
 
+import Grafico.Panel;
+import Grafico.Ventana;
+import java.awt.Color;
+import java.util.ArrayList;
+
 /**
  *
  * @author JUAN DAVID
  */
 public class main {
     public static void main(String[] args){
-        
+           Ventana nuestraventana = new Ventana("juego");
+           ArrayList ArregloDeobjetos = new ArrayList();
+           
+          /**
+           * se crea lista y dimenciones que tendra el juego
+           */
+            Panel nuestroPanel = new Panel(ArregloDeobjetos);
+                nuestraventana.add(nuestroPanel);
+                nuestraventana.setSize(800,600);
+                nuestraventana.setBackground(Color.darkGray);
+                nuestraventana.setVisible(true);
+      
     }
 }
